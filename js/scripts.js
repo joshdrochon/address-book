ec5afb8584bb0f6c3143a136f992410e8d9ce2dd
Josh Rochon and Nanette Girzi <nanette@>
2018-01-22 11:11:33 -0800

    add full name prototype

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,10 @@ function Contact(first, last) {
   this.lastName = last;
 }
 
+Contact.prototype.fullName = function(){
+  return this.firstName + " " + this.lastName;
+}
+
 
 //ui logic
 
@@ -17,7 +21,7 @@ $(document).ready(function(){
 
     var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + "</span></li>");
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
@@ -29,7 +33,5 @@ $(document).ready(function(){
       $(".last-name").text(newContact.lastName);
 
     });
-
-
   });
 });
